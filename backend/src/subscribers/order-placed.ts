988b2576140ca633cd46b810ac6e24fd83881bc5
alexisxz <alexisxzinho@hotmail.com>
2025-06-25 09:12:05 -0300

    feat: notify owner email

--- backend/src/subscribers/order-placed.ts
+++ backend/src/subscribers/order-placed.ts
@@ -1,18 +1,29 @@
-import { Modules } from '@medusajs/framework/utils'
-import { INotificationModuleService, IOrderModuleService } from '@medusajs/framework/types'
-import { SubscriberArgs, SubscriberConfig } from '@medusajs/medusa'
-import { EmailTemplates } from '../modules/email-notifications/templates'
+import {
+  INotificationModuleService,
+  IOrderModuleService,
+} from "@medusajs/framework/types";
+import { Modules } from "@medusajs/framework/utils";
+import { SubscriberArgs, SubscriberConfig } from "@medusajs/medusa";
+import { EmailTemplates } from "../modules/email-notifications/templates";
 
 export default async function orderPlacedHandler({
   event: { data },
   container,
 }: SubscriberArgs<any>) {
-  const notificationModuleService: INotificationModuleService = container.resolve(Modules.NOTIFICATION)
-  const orderModuleService: IOrderModuleService = container.resolve(Modules.ORDER)
-  
-  const order = await orderModuleService.retrieveOrder(data.id, { relations: ['items', 'summary', 'shipping_address'] })
-  const shippingAddress = await (orderModuleService as any).orderAddressService_.retrieve(order.shipping_address.id)
+  const notificationModuleService: INotificationModuleService =
+    container.resolve(Modules.NOTIFICATION);
+  const orderModuleService: IOrderModuleService = container.resolve(
+    Modules.ORDER
+  );
 
+  const order = await orderModuleService.retrieveOrder(data.id, {
+    relations: ["items", "summary", "shipping_address"],
+  });
+  const shippingAddress = await (
+    orderModuleService as any
+  ).orderAddressService_.retrieve(order.shipping_address.id);
+
+  // SENDING TO THE USER
   try {
     await notificationModuleService.createNotifications({
       to: order.email,
@@ -29,10 +40,31 @@ export default async function orderPlacedHandler({
       },
     });
   } catch (error) {
-    console.error('Error sending order confirmation notification:', error)
+    console.error("Error sending order confirmation notification:", error);
+  }
+
+  // SENDING TO THE OWNER
+  try {
+    await notificationModuleService.createNotifications({
+      to: "christianwoltz@hotmail.com",
+      channel: "email",
+      template: EmailTemplates.ORDER_PLACED,
+      data: {
+        emailOptions: {
+          replyTo: "info@fau-furnite.com",
+          subject: "A new order has been placed",
+        },
+        order,
+        shippingAddress,
+        preview: "New order for you!",
+        notifying: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error sending order notification", error);
   }
 }
 
 export const config: SubscriberConfig = {
-  event: 'order.placed'
-}
+  event: "order.placed",
+};
