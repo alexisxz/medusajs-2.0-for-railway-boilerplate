ccb118a243834797507b9446c91ff0d9071f3be3
alexisxz <alexisxzinho@hotmail.com>
2025-07-28 06:39:12 -0300

    fix: next config images unoptimized by vercel

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -1,5 +1,4 @@
 const checkEnvVariables = require("./check-env-variables")
-
 checkEnvVariables()
 
 /**
@@ -14,41 +13,50 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true, // ✅ Add this line to fix local /public image rendering
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
-        
       },
-      { // Note: needed to serve images from /public folder
-        protocol: process.env.NEXT_PUBLIC_BASE_URL?.startsWith('https') ? 'https' : 'http',
-        hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, ''),
+      {
+        protocol: process.env.NEXT_PUBLIC_BASE_URL?.startsWith("https")
+          ? "https"
+          : "http",
+        hostname: process.env.NEXT_PUBLIC_BASE_URL?.replace(/^https?:\/\//, ""),
       },
-      { // Note: only needed when using local-file for product media
+      {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL?.replace('https://', ''),
+        hostname: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL?.replace(
+          "https://",
+          ""
+        ),
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-server-testing.s3.amazonaws.com",
       },
-      { // Note: can be removed after deleting demo products
+      {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
-      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT ? [{ // Note: needed when using MinIO bucket storage for media
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_MINIO_ENDPOINT,
-      }] : []),
+      ...(process.env.NEXT_PUBLIC_MINIO_ENDPOINT
+        ? [
+            {
+              protocol: "https",
+              hostname: process.env.NEXT_PUBLIC_MINIO_ENDPOINT,
+            },
+          ]
+        : []),
     ],
   },
   serverRuntimeConfig: {
-    port: process.env.PORT || 3000
-  }
+    port: process.env.PORT || 3000,
+  },
 }
 
 module.exports = nextConfig
